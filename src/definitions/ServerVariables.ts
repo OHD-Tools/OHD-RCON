c0ea47abe6d8758c69725bd5606f86a646e65c0e
bombitmanbomb <bombitmanbomb@gmail.com>
2023-02-15 15:06:30 +0000

    Deploying to gh-pages from @ afocommunity/OHD-RCON@ae03dad1e6d2509b7ad8b09c65cfc69f6d33c575 🚀

--- src/definitions/ServerVariables.ts
+++ src/definitions/ServerVariables.ts
@@ -3,26 +3,26 @@ import type { Teams } from './Teams';
 
 export interface ServerVariables {
   Game: {
-    FriendlyFire: Readable<'0' | '1'>;
-    AutoBalanceTeamsOverride: Readable<'0' | '1'>;
+    FriendlyFire: Readable<'-1' | '1'>;
+    AutoBalanceTeamsOverride: Readable<'-1' | '1'>;
     AutoAssignHumanTeam: Readable<'-1' | Teams>;
   }
   Bot: {
-    Autofill: Readable<'0' | '1'>;
+    Autofill: Readable<'-1' | '1'>;
   }
   Net: {
     MinPlayersOverride: Readable<`${number}`>;
     MaxPlayersOverride: Readable<`${number}`>;
-    AllowServerHibernation: Readable<'0' | '1'>;
+    AllowServerHibernation: Readable<'-1' | '1'>;
   }
   HD: {
     Game: {
       MinRespawnDelayOverride: Readable<`${number}`>;
-      DisableKitRestrictionsOverride: Readable<'0' | '1'>;
+      DisableKitRestrictionsOverride: Readable<'-1' | '1'>;
     }
     CP: {
       MinPlayersToCaptureOverride: Readable<`${number}`>;
-      EnforceSmallerMinPLayersToCapture: Readable<'0' | '1'>;
+      EnforceSmallerMinPLayersToCapture: Readable<'-1' | '1'>;
     }
     AAS: {
       ActiveRouteOverride: Readable<`${number}`>
@@ -32,6 +32,3 @@ export interface ServerVariables {
 export interface UnsafeVariables {
   [key: string]: Readable<string> & UnsafeVariables
 }
-
-
-
