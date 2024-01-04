ba4296b066ebbe223886c884bbda4521bc5026db
Ethan Snow <bombitmanbomb@gmail.com>
2024-01-04 15:41:51 -0500

    Add new maps

--- src/definitions/Maps.ts
+++ src/definitions/Maps.ts
@@ -13,11 +13,15 @@ enum Maps {
    * It took place in and around the Saudi Arabian city of Khafji, from 29 January to 1 February 1991 and marked the culmination of the Coalition's air campaign over Kuwait and Iraq,
    * which had begun on 17 January 1991.
    *
-   * Factions: USA vs Russia.
+   * Factions: PMC vs Insurgents.
    *
    * ERA: Cold War
    */
   Khafji_P = 'khafji_p',
+  /**
+   * Fill out
+   */
+  Jaziira = 'jaziira',
   /**A Vietnam Map.
    *
    * Factions: USA vs NVA.
@@ -25,7 +29,14 @@ enum Maps {
    * ERA: Vietnam War
    */
   LamDong = 'lamdong',
-  /**The Battle of Monte Cassino, was a series of four assaults made by the Allies against German forces in Italy during the Italian Campaign of World War II.
+  /** Tân Bình is an urban district of Ho Chi Minh City, Vietnam. The city's international airport is situated in the district.
+   *
+   * Factions: USA vs NVA
+   *
+   * ERA: Vietnam War
+   */
+  TanBinh = 'tan_binh',
+  /** The Battle of Monte Cassino, was a series of four assaults made by the Allies against German forces in Italy during the Italian Campaign of World War II.
    * The objective was to break through the Winter Line, and facilitate an advance towards Rome.
    *
    * Factions: USA vs GERMANY
@@ -33,6 +44,20 @@ enum Maps {
    * ERA: WW2
    */
   MonteCassino = 'montecassino',
+  /**Omaha Beach was one of five beach landing sectors of the amphibious assault component of Operation Overlord during the Second World War.
+   *
+   * Factions: USA vs GERMANY
+   *
+   * ERA: WW2
+   */
+  OmahaBeach = 'omahabeach',
+  /**The Battle of Carentan was an engagement in World War II between airborne forces of the United States Army and the German Wehrmacht during the Battle of Normandy.
+   *
+   * Factions: USA vs WEHRMACHT (Germany)
+   *
+   * ERA: WW2
+   */
+  Carentan = 'carentan',
   /**The Meuse River–Argonne Forest offensive along the Western Front.
    *
    * Factions: USA vs GERMANY
@@ -40,5 +65,9 @@ enum Maps {
    * ERA: WW1
    */
   Argonne = 'argonne',
+  /**
+   *
+   */
+  SaintQuentin = 'saintquentin',
 }
 export default Maps;
