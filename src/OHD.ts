318b0ed0231fbec4676f20ea9abfff88d05ccb3f
Ethan Snow <bombitmanbomb@gmail.com>
2024-01-07 20:36:45 -0500

    Add add/remove admin commands
    Co-Authored-By: ohathar <6678917+ohathar@users.noreply.github.com>

--- src/OHD.ts
+++ src/OHD.ts
@@ -15,6 +15,7 @@ import {
 } from './definitions/ServerVariables';
 import VariableChanged from './definitions/VariableChanges';
 import Player from './Player';
+import CommandSuccess from './definitions/CommandSuccess';
 
 enum PacketType {
   COMMAND = 0x02,
@@ -420,6 +421,20 @@ export default class OHD {
       };
     });
   }
+  public addAdmin(user: string | number) {
+    if (typeof user === 'number') return this.addAdminById(user);
+    return this.send<CommandSuccess>(`admin add "${user}"`);
+  }
+  public addAdminById(id: number) {
+    return this.send<CommandSuccess>(`admin addId ${id}`);
+  }
+  public removeAdmin(user: string | number) {
+    if (typeof user === 'number') return this.removeAdminById(user);
+    return this.send<CommandSuccess>(`admin remove "${user}"`);
+  }
+  public removeAdminById(id: number) {
+    return this.send<CommandSuccess>(`admin removeId ${id}`);
+  }
   /**
    * Add `amount` bots to the server.
    */
@@ -516,9 +531,10 @@ export default class OHD {
    * Kick a `Player` from the server by Username
    */
   public async kick(
-    name: string,
+    name: string | number,
     reason = 'You have been Kicked',
   ): Promise<PlayerKicked> {
+    if (typeof name === 'number') return this.kickId(name, reason);
     const kicked = await this.send<PlayerKicked>(`kick "${name}" "${reason}"`);
     if (kicked.success) {
       const player = [...this.players.entries()].find((p) => p[1].name == name);
@@ -550,10 +566,11 @@ export default class OHD {
    * Ban a `Player` from the server by Username.
    */
   public async ban(
-    name: string,
+    name: string | number,
     /** Duration in Seconds*/ duration = 0,
     reason?: string,
   ): Promise<PlayerBanned> {
+    if (typeof name === 'number') return this.banId(name, duration, reason);
     if (reason == null)
       reason =
         duration == 0
